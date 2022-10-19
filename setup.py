b4de4e6623732713ba0a8e6af1e3b15d865950c2
Subhasish Saha <subhasishsaha007@gmail.com>
2022-10-20 02:00:42 +0530

    Project initialized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "sensor",
+    version = "0.0.1",
+    author = "subhasish",
+    author_email = "subhasishsaha007@gmail.com",
+    packages = find_packages(),
+)
+
