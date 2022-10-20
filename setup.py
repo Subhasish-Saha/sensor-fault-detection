679c5bd4c3b4d81ed269643d18356460a856063b
Subhasish Saha <subhasishsaha007@gmail.com>
2022-10-20 22:57:16 +0530

    Created the folder structure

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 from setuptools import find_packages,setup
+from typing import List, list
+
+def get_requirements()->List[str]:
+
+    """
+    This function will return the list of requirements
+    """
+
+    requirement_list:List[str]=[]
+
+    """
+    Write code to read requirements.txt file and append each requirements in requirement_list variable.
+    """
+
+    return requirement_list
 
 setup(
     name = "sensor",
@@ -6,5 +21,7 @@ setup(
     author = "subhasish",
     author_email = "subhasishsaha007@gmail.com",
     packages = find_packages(),
+    install_requires=get_requirements(),
 )
 
+# python setup.py install
