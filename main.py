fdc5b4eb4fa06d072961bfc2b5c8cbafd2d0c922
Subhasish Saha <subhasishsaha007@gmail.com>
2022-11-07 22:20:16 +0530

    Data ingestion bug fixed

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     
     training_pipeline = TrainPipeline()
     training_pipeline.run_pipeline()
+
--- sensor/data_access/sensor_data.py
+++ sensor/data_access/sensor_data.py
@@ -47,9 +47,13 @@ class SensorData:
             return pd.DataFrame of collection
             """
             if database_name is None:
+                # collection = self.mongo_client.database['car']
                 collection = self.mongo_client.database[collection_name]
+                print('Executing if block : ',collection)
+                
             else:
                 collection = self.mongo_client[database_name][collection_name]
+                print('Executing else block : ',collection)
             df = pd.DataFrame(list(collection.find()))
 
             if "_id" in df.columns.to_list():
